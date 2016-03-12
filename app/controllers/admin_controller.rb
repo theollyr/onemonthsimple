daf0dcbdf2d917685a95f98e8d38255514ba0e30
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2016-03-13 00:08:27 +0100

    remove user ID 1 exception on AdminController
    It caused that anybody could access /admin/1/dashboard
    and manage the other users.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,5 @@
 class AdminController < ApplicationController
-  before_filter :administrative, :if => :admin_param, :except => [:get_user]
+  before_filter :administrative
   skip_before_filter :has_info
 
   def dashboard
