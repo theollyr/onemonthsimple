3a764de887ee407c8763a7064937e6b3336a1484
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2016-03-12 23:37:26 +0100

    fix WorkInfo controller that wasn't using current_user
    Instead, it used the user ID from params to access the data which
    created a security issue because anyone could access the data of
    other users through this controller.

--- app/controllers/work_info_controller.rb
+++ app/controllers/work_info_controller.rb
@@ -1,6 +1,6 @@
 class WorkInfoController < ApplicationController
   def index
-    @user = User.find_by_user_id(params[:user_id])
+    @user = current_user
     if !(@user) || @user.admin
       flash[:error] = "Sorry, no user with that user id exists"
       redirect_to home_dashboard_index_path
