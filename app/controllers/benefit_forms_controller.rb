97e0041746b3257ea4d02bda31b35d18f0dc771c
Patrik Gajdošík <gajdosik.patrikk@gmail.com>
2016-04-16 15:53:32 +0200

    fix insecure download by allowing only specific files

--- app/controllers/benefit_forms_controller.rb
+++ app/controllers/benefit_forms_controller.rb
@@ -1,16 +1,18 @@
 class BenefitFormsController < ApplicationController
+  DOC_DIR = File.join('public', 'docs')
+  DOC_FILES = Dir[File.join(DOC_DIR, '*')]
 
   def index
     @benefits = Benefits.new
   end
 
   def download
-   begin
-     file = params[:name]
-     send_file file, :disposition => 'attachment'
-   rescue
-     redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
-   end
+    if DOC_FILES.include?(params[:name])
+      file = Rails.root.join(params[:name])
+      send_file file, :disposition => 'attachment'
+    else
+      redirect_to user_benefit_forms_path(:user_id => current_user.user_id)
+    end
   end
 
   def upload
